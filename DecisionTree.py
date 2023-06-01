20aa606e8574da102fcf2270d8c34be06021c651
tyrellbaker-blip <91085471+tyrellbaker-blip@users.noreply.github.com>
2023-06-01 13:13:35 -0700

    Lesson Instructions

---
+++ DecisionTree.py
@@ -0,0 +1,35 @@
+# TODO: We're going to implement a different algorithm, decision trees:
+# TODO: IMPORTS(BELOW)
+'''
+pandas for data manipulation.
+sklearn.tree for creating decision tree models.
+sklearn.model_selection for splitting the data into training and testing
+sets.
+sklearn.metrics for evaluating the performance of the model.
+'''
+
+# TODO: Load the data from the CSV file into a pandas dataframe using the
+#  read_csv() function. Preprocess the data by: Removing unnecessary columns
+#  from the dataset.v Cleaning the data (i.e., imputing missing values,
+#  removing outliers, and scaling the features). Transforming categorical
+#  variables into one-hot encoded numerical variables (if necessary). This
+#  can be done manually or by calling a pre-written function such as the
+#  preprocess_data() function from data_processor.py Look in that module for
+#  an explanation of how to use it.
+#
+
+#
+# TODO: Define the target variable for classification. In this case,
+#  the target variable is "popular".
+#
+# TODO: Split the data into input and target variables using the drop() method.
+#
+# TODO: Split the data into training and testing sets using the
+#  train_test_split() function.
+#
+# TODO: 1. Create a DecisionTreeClassifier object.
+# TODO: 2. Fit the classifier to the training data using the fit() method.
+# TODO: 3. Make predictions on the test data  using the predict() method.
+# TODO: 4. Evaluate the performance of the model using the accuracy_score()
+#  function.
+# TODO: 5. Print the accuracy of the model.
