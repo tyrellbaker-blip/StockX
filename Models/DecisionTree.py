7fa697af9762b96e8c85069b41ff472de7434a4e
prameet <prameet.guha@gmail.com>
2023-06-02 22:01:56 -0400

    Hello Mr. Baker! I can't seem to call any functions from data_preprocessor.py. On line 40 of DecisionTree.py, I tried clean_data, but it isn't working. Can you please help me?

--- Models/DecisionTree.py
+++ Models/DecisionTree.py
@@ -1,5 +1,11 @@
 # TODO: We're going to implement a different algorithm, decision trees:
 # TODO: IMPORTS(BELOW)
+import pandas as pd
+import sklearn.tree
+import sklearn.model_selection
+import sklearn.metrics
+from data_preprocessor import clean_data
+
 '''
 pandas for data manipulation.
 sklearn.tree for creating decision tree models.
@@ -18,12 +24,27 @@ sklearn.metrics for evaluating the performance of the model.
 #  an explanation of how to use it.
 #
 
+# a. Read & Loaded original data into a dataframe
+df = pd.read_csv('../resources/stockx.csv')
+#print(df.head(5))
+#column_headers_original = list(df.columns.values)
+#print("The Original Column Header :", column_headers_original)
+
+# b. Dropping unnecessary columns
+df_1 = df.drop(['Order Date', 'Retail Price', 'Release Date', 'Shoe Size', 'Buyer Region'], axis=1)
+column_headers = list(df_1.columns.values)
+print("The Column Header after dropping columns :", column_headers)
+print(df_1.head(10))
+
+# c. Cleaning the dataframe
+cleanedDataframe = clean_data(df_1)
+
 #
 # TODO: Define the target variable for classification. In this case,
 #  the target variable is "popular".
 #
 # TODO: Split the data into input and target variables using the drop() method.
-#
+
 # TODO: Split the data into training and testing sets using the
 #  train_test_split() function.
 #
