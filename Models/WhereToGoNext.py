1281890aa3d69bdbf6d053977b4d4fbe649453ca
tyrellbaker-blip <91085471+tyrellbaker-blip@users.noreply.github.com>
2023-06-06 14:01:33 -0700

    Added TODOs

--- Models/WhereToGoNext.py
+++ Models/WhereToGoNext.py
@@ -1,1 +1,9 @@
-#TODO:
+#TODO: Build a function to calculate purchase date/ release date difference?
+
+#TODO: Total value of shoes sold over their retail value?
+
+#TODO: How long after the release date was the shoe sold?
+
+##TODO: What are the most and least popular shoes within each category? If
+# we can figure this out, we can figure out which values to exclude from our
+# model
