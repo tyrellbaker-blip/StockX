83318171feb9fd9fc23fbc296e060250e2d84481
prameet <prameet.guha@gmail.com>
2023-06-06 21:24:06 -0400

    What I did so far.

--- Models/WhereToGoNext.py
+++ Models/WhereToGoNext.py
@@ -1,9 +1,14 @@
 #TODO: Build a function to calculate purchase date/ release date difference?
+#   In our function, we would first compare the year as an int, then month as
+#   an int, then day as an int, unless there is already a function to compare
+#   dates
 
 #TODO: Total value of shoes sold over their retail value?
 
 #TODO: How long after the release date was the shoe sold?
+#   Along with this, we can also see how often they were sold, or in other words,
+#   what was the time period between each sell?
 
-##TODO: What are the most and least popular shoes within each category? If
-# we can figure this out, we can figure out which values to exclude from our
-# model
+#TODO: What are the most and least popular shoes within each category? If
+#   we can figure this out, we can figure out which values to exclude from our
+#   model
