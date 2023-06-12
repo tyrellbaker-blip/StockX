5394faf7b43df0fc87a11c6424276273010a896c
prameet <prameet.guha@gmail.com>
2023-06-12 18:40:57 -0400

    Add link and info on how to arrange profit

--- Models/WhereToGoNext.py
+++ Models/WhereToGoNext.py
@@ -2,7 +2,6 @@ import pprint
 import csv
 from datetime import datetime
 
-
 def get_days_between_release_and_sale(csv_file):
     with open(csv_file, 'r', encoding='utf-8-sig') as f:
         reader = csv.DictReader(f)
@@ -17,13 +16,32 @@ def get_days_between_release_and_sale(csv_file):
 
     return days_between
 
-
 # TODO: Build a function to calculate purchase date/ release date difference?
 #   In our function, we would first compare the year as an int, then month as
 #   an int, then day as an int, unless there is already a function to compare
 #   dates
 
 # TODO: Total value of shoes sold over their retail value?
+#   PLANNING:
+#   https://stackoverflow.com/questions/48906098/adding-numbers-in-a-csv-file-python-code
+#   https://www.w3schools.com/python/python_dictionaries_add.asp
+#   https://www.w3resource.com/python-exercises/dictionary/python-data-type-dictionary-exercise-1.php
+#   All we need is shoe model, original price, and retail price. So...
+#   ----------------------------------------------------------------------------------------------------------
+#   dataframe = dataframe.drop(['Order Date', 'Brand', 'Release Date', 'Shoe Size', 'Buyer Region'], axis=1)
+#   profit = {}
+#   for each shoe model:
+#       retailSum = sum('Retail Price' of each pair sold)
+#       saleSum = sum('Sale Price' of each pair sold)
+#       profit['Sneaker Name'] = str(saleSum - retailSum)
+#   print(profit)
+#   def sort_dict_by_value(d, reverse=False): <-- arranging so the values are in ascending order.
+#       return dict(sorted(d.items(), key=lambda x: x[1], reverse=reverse))
+#   sort_dict_by_value(profit)
+#   arrange/sort profit from greatest to least.
+#   print("Most Profit" + str(profit[5])
+#   print("Least Profit" + str(profit[-5])
+#
 
 # TODO: How long after the release date was the shoe sold?
 #   Along with this, we can also see how often they were sold, or in other
